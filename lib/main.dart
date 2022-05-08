f1de4caf67a0c0eb91aa979bf0a0aa85801a1c80
k-aoshima <zakuzaku083@gmail.com>
2022-05-08 22:03:38 +0900

    Add DarkMode

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,53 @@ import 'package:flutter/material.dart';
 import 'routeInfo.dart';
 import 'lineInfo.dart';
 import "dart:async";
+import 'package:provider/provider.dart';
+
 
 
 RouteInfo _routeInfo = RouteInfo();
 LineInfo _lineInfo = LineInfo();
 
-void main(){
-  runApp(const MyApp());
+void main() =>  
+  runApp(
+    ChangeNotifierProvider(
+		create: (_) => AppTheme(),
+		child: const MyApp(),
+	),
   
-}
+);
+
+
+class AppTheme extends ChangeNotifier {
+	AppTheme() : _isDark = false;
+
+	bool get isDark => _isDark;
+
+	bool _isDark;
+	
+	ThemeData buildTheme()
+		=> _isDark ? ThemeData.dark() : ThemeData.light();
+	
+	void changeMode() {
+		_isDark = !_isDark;
+		notifyListeners();
+	}
+} 
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({Key? key}) : super(key: key);
-
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    
+
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: Provider.of<AppTheme>(context).buildTheme(),
       home: FutureBuilder(
         future:_getFutureData(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -40,6 +65,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -107,16 +133,12 @@ class TrianglePainter extends CustomPainter {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   
   void _incrementCounter() {
     
     _routeInfo.abbreviation[0];
     _lineInfo.latitude[0];
-
-    setState(() {
-      _counter++;
-    });
+    
   }
 
   double _nextStationFontSize(String nextStationName){
@@ -131,13 +153,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Provider.of<AppTheme>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: TextStyle(fontFamily: "BIZUDPGothic", fontSize: 20, fontWeight: FontWeight.bold),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(padding: EdgeInsetsDirectional.fromSTEB(50, 0, 10, 0),
+              child: Stack(
+                alignment: const Alignment(0, 0),
+                children: [
+                  Container(
+                    width: 40,
+                    height: 25,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  Text(_routeInfo.abbreviation[0],
+                    style: TextStyle(fontFamily: "BIZUDPGothic", fontSize: 15, fontWeight: FontWeight.bold, color: _routeInfo.routeColor[0])
+                  ),
+                ],
+              )
+            ),
+            Text(
+              widget.title,
+              style: TextStyle(fontFamily: "BIZUDPGothic", fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
         backgroundColor: _routeInfo.routeColor[0],
+        
       ),
       body: SafeArea(
         child: Row(
@@ -212,7 +260,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 30),
                         child: 
                         Text(
-                          _lineInfo.stationName[20],
+                          _lineInfo.stationName[2],
                           style: TextStyle(fontFamily: "BIZUDPGothic", fontSize: _nextStationFontSize(_lineInfo.stationName[2]))
                         ),
                       ),
@@ -268,19 +316,42 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: EdgeInsets.zero,
               children: [
                 DrawerHeader(
-                  child: Text(
-                    '路線：' + _routeInfo.routeName[0],
-                    style: const TextStyle(
-                      fontFamily: "BIZUDPGothic",
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 17.0
-                    ),
-                  ),
                   decoration: BoxDecoration(color: _routeInfo.routeColor[0]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 0, 70, 10),
+                        child: Stack(
+                          alignment: const Alignment(0, 0),
+                          children: [
+                            Container(
+                              width: 60,
+                              height: 45,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                            Text(_routeInfo.abbreviation[0],
+                              style: TextStyle(fontFamily: "BIZUDPGothic", fontSize: 20, fontWeight: FontWeight.bold, color: _routeInfo.routeColor[0])
+                            ),
+                          ],
+                        )
+                      ),
+                      Text(
+                        '路線：' + _routeInfo.routeName[0],
+                        style: const TextStyle(
+                          fontFamily: "BIZUDPGothic",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 17.0
+                        ),
+                      ),  
+                    ],
+                  ),
                 ),
+                
                 ListTile(
-                  
                   title: const Text(
                     '路線設定',
                     style: TextStyle(
@@ -300,16 +371,29 @@ class _MyHomePageState extends State<MyHomePage> {
                     Navigator.pop(context);
                   }
                 ),
-                ListTile(
-                  title: const Text(
-                    'ダークモード',
-                    style: TextStyle(
-                    fontFamily: "BIZUDPGothic"
-                  )),
-                  onTap:(){
-                    Navigator.pop(context);
-                  }
-                ),
+                Stack(
+                  children: [
+                    ListTile(
+                      title: const Text(
+                        'ダークモード',
+                        style: TextStyle(
+                        fontFamily: "BIZUDPGothic"
+                      )),
+                    ),
+                    Positioned(
+                      left: 130,
+                      top: 4,
+                      child: Switch.adaptive(
+                        activeColor: _routeInfo.routeColor[0],
+                        value: theme.isDark,
+                        onChanged: (_){
+                        setState(() {
+                          theme.changeMode();
+                        });
+                      }),
+                    )
+                  ],
+                )
               ],
             ),
           ),
@@ -321,6 +405,7 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(
             Icons.gps_fixed,
+            color: Colors.white,
             size: 35.0,
           )
       ),
